014592441cc67a03fdd13e78310d8b7b2623a869
Shivani21-hude <shivani21hude@gmail.com>
2023-02-18 19:58:44 +0530

    [Shivani]-Read the inventory file of json into class

--- InventoryDataManagement/DataManagement.cs
+++ InventoryDataManagement/DataManagement.cs
@@ -12,7 +12,7 @@ namespace InventoryDataManagement
     {
         public void DataInventory()
         {
-            string filepath = "E:\\RFP 215\\InventoryDataManagement\\InventoryDataManagement\\DataInventory.json";
+            string filepath = "E:\\RFP 215\\InventoryDataManagement\\JSON_DataInventoryManagement\\InventoryDataManagement\\DataInventory.json";
             try
             {
                 if (File.Exists(filepath))
@@ -20,7 +20,7 @@ namespace InventoryDataManagement
                     string record=File.ReadAllText(filepath);
                     Data data =JsonConvert.DeserializeObject<Data>(record);
                     List<Inventory> inventories = data.inventories;
-                    foreach(Inventory items in inventories)
+                    foreach (Inventory items in inventories)
                     {
                         double value = items.Price * items.Weight;
                         Console.WriteLine("Name : " + items.Name + ",  Weight : " + items.Weight + ",  Price :  " + items.Price + ", Total Value :" + value);
